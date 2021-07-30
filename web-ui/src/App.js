03780754d22fc497aea92eeec1a42e00d5188884
Melbin Mathew <mmathew@trl.co.uk>
2021-07-31 00:19:42 +0530

    edit demo

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited for demo
+          Edited for demo- check commit in vscode
         </p>
         <a
           className="App-link"
