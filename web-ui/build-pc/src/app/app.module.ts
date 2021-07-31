2f1f5cc66c857ee91d79ff3005c54ee5613034af
Melbin Mathew <mmathew@trl.co.uk>
2021-07-31 22:32:32 +0530

    added homepage, header and footer components

--- web-ui/build-pc/src/app/app.module.ts
+++ web-ui/build-pc/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './features/home-page/home-page.component';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
